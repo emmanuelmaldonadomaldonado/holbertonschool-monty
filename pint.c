4dce7c6eb9cc057b11c677a654e74f3a15dbfa19
millienytorressanta <tmillieny@gmail.com>
2024-01-04 01:41:40 -0400

    probando codigo

--- pint.c
+++ pint.c
@@ -11,15 +11,15 @@ void f_pint(stack_t **head, unsigned int number)
 
 	if (*head == NULL)
 
-	{
+        {
 
-		fprintf(stderr, "L%u: cant't pint, stack empty\n", number);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+                fprintf(stderr, "L%u: cant't pint, stack empty\n", number);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
 
-		}
+                }
 
-	printf("%d\n", (*head)->n);
-					}
+        printf("%d\n", (*head)->n);
+                                        }

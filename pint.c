ea6ff86e632b7d0af3040e31c77c91775cf786ed
millienytorressanta <tmillieny@gmail.com>
2024-01-07 06:42:19 -0400

    files

--- pint.c
+++ pint.c
@@ -8,11 +8,11 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		cleanup(NULL, NULL, *stack);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*stack)->n);
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        cleanup(NULL, NULL, *stack);  // Pass appropriate arguments to cleanup
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
 }

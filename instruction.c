24d31546f437104b1e8523d60bfc04bda0ffcafe
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-10 14:24:38 -0400

    Reverted file

--- instruction.c
+++ instruction.c
@@ -1,24 +1,24 @@
-#include "instructions.h"
+#include "monty.h"
+#include <string.h>
 
-/**
- * get_instructions - Get the array of instructions
- *
- * Return: Pointer to the array of instructions
- */
+instruction_t instructions[] = {
+    {"push", push},
+    {"pall", pall},
+    {"pint", pint},
+    {"pop", pop},
+    {"swap", swap},
+    {"add", add},
+    {NULL, NULL} // Marcar el final del array
+};
 
 const instruction_t *get_instruction(char *opcode)
 {
-	static instruction_t my_instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"nop", nop},
-		{"swap", swap},
-		{"add", add},
-		{NULL, NULL}
-	};
-
-	return (my_instructions);
+    for (int i = 0; instructions[i].opcode; i++)
+    {
+        if (strcmp(instructions[i].opcode, opcode) == 0)
+        {
+            return &instructions[i];
+        }
+    }
+    return NULL; // Instrucción no encontrada
 }
-

2e9cbc5084de183b96fb8e01685f7fc30bc1717c
millienytorressanta <tmillieny@gmail.com>
2024-01-10 22:52:21 -0400

    codigo del principio con cambios

--- cleanup.c
+++
@@ -1,9 +0,0 @@
-#include "monty.h"
-
-void cleanup_resources(FILE *file, char *line, stack_t **stack)
-{
-    free(line);
-    fclose(file);
-    free_stack(stack);
-}
-
--- main.c
+++ main.c
@@ -1,52 +1,41 @@
 #include "monty.h"
-#include "instructions.h"
-#include "my_file.h"
 
+global_t var;
 /**
- * main - Entry point for Monty interpreter
- * @argc: Number of arguments
- * @argv: Array of arguments passed to the program
+ *  main -  interpreter for Monty ByteCodes files
+ *  @argc: Number of paramethers
+ *  @argv: Pointer to all the paramethers
  *
- * Return: Always 0 on success
+ *  Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	instruction_t *my_instructions = get_instructions();
-	FILE *file = fopen(argv[1], "r");
-	char *line = NULL;
-	char *opcode = NULL;
-	size_t len = 0;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
-	int i;
+	size_t line_buf_size = 0;
 
-		if (argc != 2)
+	var.getl_info = NULL;
+	var.stack_head = NULL;
+	var.n_lines = 0;
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-		my_file(file, argv);
-		while ((getline(&line, &len, file)) != -1)
+	var.fp_struct = fopen(argv[1], "r");
+	if (!var.fp_struct)
 	{
-		line_number++;
-		opcode = strtok(line, " \n");
-		if (opcode == NULL || *opcode == '#')
-			continue;
-		for (i = 0; my_instructions[i].opcode != NULL; i++)
-		{
-			if (strcmp(opcode, my_instructions[i].opcode) == 0)
-			{
-				my_instructions[i].f(&stack, line_number);
-				break;
-			}
-		}
-		if (my_instructions[i].opcode == NULL)
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-			cleanup_resources(file, line, &stack);
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-		cleanup_resources(file, line, &stack);
-	return (0);
+	while (getline(&var.getl_info, &line_buf_size, var.fp_struct) != EOF)
+	{
+		var.n_lines++;
+		if (line_validator(var.getl_info) == 1)
+			continue;
+		/*split_str(var.getl_info);*/
+		execute_opcode(split_str(var.getl_info));
+	}
+	free(var.getl_info);
+	handle_dlist_head(var.stack_head);
+	fclose(var.fp_struct);
+	return (EXIT_SUCCESS);
 }

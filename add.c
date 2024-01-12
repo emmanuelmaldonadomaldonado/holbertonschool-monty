d9f5f5b09f937c537f05b5366f805bf36d89d2ad
millienytorressanta <tmillieny@gmail.com>
2024-01-12 04:26:46 -0400

    trying

--- add.c
+++ add.c
@@ -5,7 +5,6 @@
 
 void add(stack_t **stack, unsigned int line_number)
 {
-
     if (*stack == NULL || (*stack)->next == NULL)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
@@ -13,5 +12,6 @@ void add(stack_t **stack, unsigned int line_number)
     }
 
     (*stack)->next->n += (*stack)->n;
-    pop(stack, line_number);
+    pop(stack, line_number); // Include the third argument (value)
 }
+
